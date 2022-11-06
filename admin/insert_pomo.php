180ea9db972e7138e42f9c01d74eecb0ae3baf75
youchi003 <saiyutee.tama01@gmail.com>
2022-11-06 16:11:53 +0700

    6/11/2565

--- admin/insert_pomo.php
+++ admin/insert_pomo.php
@@ -1,6 +1,8 @@
 <?php
 include 'condb.php';
 $detail_pomo =$_POST['pomo'];
+$pomoname= $_POST['pomoname'];
+$pomosale= $_POST['pomosale'];
 //อัพโหลดรูปภาพ
 if (is_uploaded_file($_FILES['file2']['tmp_name'])) {
     $new_image_name = 'po_'.uniqid().".".pathinfo(basename($_FILES['file2']['name']), PATHINFO_EXTENSION);
@@ -9,8 +11,8 @@ if (is_uploaded_file($_FILES['file2']['tmp_name'])) {
     } else {
     $new_image_name = "";
     }
-    $sql="INSERT INTO promotion(pomo_img,detail_pomo) 
-    VALUES('$new_image_name','$detail_pomo') ";
+    $sql="INSERT INTO promotion(pomo_img,detail_pomo,pomo_name,pomo_sale) 
+    VALUES('$new_image_name','$detail_pomo','$pomoname','$pomosale') ";
     $result=mysqli_query($conn,$sql);
     if($result){
         echo "<script> alert('บันทึกข้อมูลเรียบร้อย');</script>";
